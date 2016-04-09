d63d8eea60334b44fac732efb30ed6ab06a3fd27
Aaron <Aaron.r.grau@gmail.com>
2016-04-09 19:06:24 -0400

    move analytics

--- frontend/kitchen_starter.jsx
+++ frontend/kitchen_starter.jsx
@@ -1,3 +1,12 @@
+//google analtyics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-76213914-1', 'auto');
+ga('send', 'pageview');
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
@@ -23,6 +32,7 @@ var Home = require('./components/restaurants/home');
 var SessionStore = require('./stores/session_store');
 var ApiUtil = require('./util/api_util');
 var RestaurantActions = require('./actions/restaurant_actions.js');
+
 
 var App = React.createClass({
 
